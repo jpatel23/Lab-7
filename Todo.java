536dcf37d3f680c940be82ec1c938f0f1c102bbe
Jaimin Patel <jaiminabpatel@ou.edu>
2019-03-11 18:59:29 -0500

    Created todo list

---
+++ Todo.java
@@ -0,0 +1,111 @@
+
+public class Todo {
+/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-21
+OU ID_______________: 113411744
+OU EMAIL____________: jaiminabpatel@ou.edu
+
+Tasks:
+1. Create StackHospital class
+2. Create QueueHospital class
+3. Create PriorityQueueHospital class
+4. Create HealthyPerson class
+5. Create SickPerson class
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
+12. 
+13. 
+15. 
+16. 
+17. 
+18. 
+19. 
+20. 
+21. 
+22. 
+23. 
+24. 
+25. 
+26. 
+27. 
+28. 
+29. 
+30. 
+31.
+32. 
+33. 
+
+Estimated Time (minutes):
+1. 3
+2. 3
+3. 3
+4. 3
+5. 3
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
+12. 
+13. 
+14. 
+15. 
+16. 
+17.  
+18. 
+19. 
+20. 
+21. 
+22. 
+23. 
+24. 
+25. 
+26. 
+27. 
+28. 
+29. 
+30. 
+31. 
+32. 
+33. 
+Actual Time (minutes):
+1. 1
+2. 1
+3. 1
+4. 1
+5. 1
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
+12. 
+13. 
+14. 
+15. 
+16. 
+17. 
+18. 
+19. 
+20. 
+21. 
+22. 
+23. 
+24. 
+25. 
+26. 
+27. 
+28. 
+29. 
+30. 
+31. 
+32. 
+33. 
+*/}
---
+++ src/StackHospital.java
@@ -0,0 +1,40 @@
+
+public class StackHospital<PatientType> extends Hospital<PatientType> {
+
+	@Override
+	public void addPatient(PatientType patient) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public PatientType nextPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PatientType treatNextPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numPatients() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String hospitalType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String allPatientInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
